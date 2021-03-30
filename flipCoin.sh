65bb10bdad0faa7d1cbd0538dd6af36958e85844
namdev1994 <n.lavhare1994@gmail.com>
2021-03-30 14:58:47 +0530

    Added:Flipping coin in Multiple time Result in Dictionary

--- flipCoin.sh
+++ flipCoin.sh
@@ -1,9 +1,26 @@
-random=$((RANDOM%2))
+echo -e "enter the times to toss coin \c"
+read num
+Head=0
+Tail=0
 
-if [ $random -eq 1 ]
-then
-	 echo "Head"
-else
-	echo "tail"
-fi
+for ((i=0; i<$num; i++))
+do
+	random=$((RANDOM%2))
+	if [ $random -eq 1 ]
+	then
+		 ((Head++))
+	else
+		 ((Tail++))
+	fi
+done
 
+declare -A Coins
+Coins[((Head))]=$Head
+Coins[((Tail))]=$Tail
+echo ${Coins[@]}
+
+h=$(($Head*100))
+headper=$(($h/$num))
+t=$(($Tail*100))
+tailper=$(($t/$num))
+echo "head % : $headper , tails % : $tailper "
